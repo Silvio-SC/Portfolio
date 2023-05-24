1d71be6afaf572da2c9be130036ec75c74eb059b
Silvio-SC <vinhoterranossa@hotmail.com>
2023-05-24 10:33:24 -0300

    cabo

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,5 +1,8 @@
 import { user } from '../../data/user.js'
 import styles from './styles.module.css'
+import whatsapp from '../../assets/whatsapp-icon.png'
+import linkedin from '../../assets/linkedin-icon.png'
+import github from '../../assets/github-icon.png'
 
 export const Footer = () => {
     return (
@@ -7,9 +10,9 @@ export const Footer = () => {
         <div>
           <h2>Contato</h2>
           <div>
-            <a href='#'><img src="./src/assets/whatsapp-icon.png" alt="" /></a>
-            <a href='#'><img src="./src/assets/linkedin-icon.png" alt="" /></a>
-            <a href='#'><img src="./src/assets/github-icon.png" alt="" /></a>
+            <a href='#'><img src={whatsapp} alt="icon do whatsapp" /></a>
+            <a href='#'><img src={linkedin} alt="icone do linkedin" /></a>
+            <a href='#'><img src={github} alt="icone do github" /></a>
           </div>
         </div>
         <p>Todos os direitos reservados - {user}</p>
